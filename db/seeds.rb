f3909ab4a0021a047b2665590db143736e399449
Daniel Carvalho <danielbpc2@hotmail.com>
2018-03-29 12:29:19 -0300

    Controllers and routes created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+puts "seeding"
+
+# parse = JSON.parse(open("https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list").read)
+
+# parse["drinks"].each do |x|
+#   x.each do |k, v|
+#     Ingredient.create(name: v)
+#   end
+# end
+
+# puts "ok"
