089601b42fecb73c4368a384c4e81f36bf167813
Daniel Carvalho <danielbpc2@hotmail.com>
2018-03-29 14:21:59 -0300

    Cock controller made

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,13 +1,27 @@
 class CocktailsController < ApplicationController
+  before_action :set_cocktail, only: [:show]
   def index
+    @cocktails = Cocktail.all
   end
 
   def show
   end
 
   def new
+    @cocktail = Cocktail.new
   end
 
   def create
+    @cocktail = Cocktail.new(cock_params)
+  end
+
+  private
+
+  def cock_params
+    params.require(:cocktails).permit(:name)
+  end
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:id])
   end
 end
